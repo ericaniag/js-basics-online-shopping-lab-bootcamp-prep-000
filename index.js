3dfd67acae9a972686ee0c13081bd596962f2a72
Eric Aniag <ericaniag@gmail.com>
2019-05-04 22:48:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,14 +41,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
-      cart.splice(i,1)
-      return cart
-    } else {
-      
-    }
-  }
+  
 }
 
 function placeOrder(cardNumber) {
